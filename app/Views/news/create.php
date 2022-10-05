5dc43c4d17a4730f34112bd47406cfb366c2550b
humphreyokoth <humpasp@gmail.com>
2022-10-05 13:07:16 +0300

    form

---
+++ app/Views/news/create.php
@@ -0,0 +1,14 @@
+<h2><?=esc($title)?></h2>
+<?=session()->getFlashdata('error')?>
+<?=service('validation')->listErrors('error')?>
+
+<form action="/news/create" method="post">
+    <?=csrf_field()?>
+    <label for="titel">Title</label>
+    <input type="input" name="title"/><br/>
+    <label for="body">Text</label>
+    <textarea name="body" cols="45" rows="4"></textarea><br/>
+
+    <input type="submit" name="submit" value="Create news item"/>
+
+</form>
